ab119b2fb7abe0e129e67ed03255c248714a02ff
Adel <talk2adel@yahoo.com>
2022-12-29 14:19:50 +0100

    Added time zone

--- src/app.js
+++ src/app.js
@@ -1,15 +1,35 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHour();
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days =[
+        "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
-console.log(response.data.main.temp)
+console.log(response.data)
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description")
+let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
-let windElement = document.querySelector("#wind")
+let windElement = document.querySelector("#wind");
+let dateElement = document.querySelector("#date");
+
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
-humidityElement.innerHTML = response.data.humidity;
+humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
+dateElement.innerHTML = response.data.coord.timezone
 }
 
 
