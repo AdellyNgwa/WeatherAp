8dbe214e8310088be2af0824b3faef49fffe6ffc
Adel <talk2adel@yahoo.com>
2022-12-30 23:36:54 +0100

    Added weather icon

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
 
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
@@ -29,6 +30,11 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt *1000)
+iconElement.setAttribute(
+    "src",
+     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+     );
+     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
