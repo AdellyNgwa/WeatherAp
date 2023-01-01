e0e192bbb666e85e1891548717e02043338b985e
Adel <talk2adel@yahoo.com>
2023-01-01 23:54:20 +0100

    Added daily forecast data

--- src/app.js
+++ src/app.js
@@ -14,33 +14,43 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+    return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily)
+    let forecast = response.data.daily;
 
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
-    let days = ["Mon","Tue","Wed","Thr","Fri","Sat"];
-    days.forEach(function(day) {
+    
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
         forecastHTML = forecastHTML + `
         <div class="col-2">
             <div class="weather-forecast-date">
-            ${day}
+            ${formatDay(forecastDay.dt)}
             </div>
-            <img src="http://openweathermap.org/img/wn/50d@2x.png" alt=""
+            <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt=""
             width="40"/>
     
             <div class="weather-forecast-temperatures">
                 <span class="weather-forecast-temperature-max">
-                    30°
+                    ${Math.round(forecastDay.temp.max)}°
                 </span>
                 <span class="weather-forecst-temperature-min">
-                    19°
+                    ${Math.round(forecastDay.temp.min)}°
                 </span>
     
             </div>
         </div>
     `;
+        }
     })
    
 forecastHTML = forecastHTML + `</div>`;
@@ -49,7 +59,6 @@ forecastHTML = forecastHTML + `</div>`;
 }
 
 function getForcast(coordinates) {
-    console.log(coordinates)
     let apiKey = "2bd326a60dc89a53287e446e819664df";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     console.log(apiUrl);
